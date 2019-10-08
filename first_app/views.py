b4f518d2aabc29000beb5e5e47215b999bd6fd0e
jinja27 <17754970254@163.com>
2019-10-08 23:06:15 +0800

    “第三次提交，显示文章内容和标题

--- first_app/views.py
+++ first_app/views.py
@@ -5,4 +5,6 @@ from .models import Article
 # Create your views here.
 def article_detail(request,article_id):
     article=Article.objects.get(id=article_id)
-    return HttpResponse("文章id为：%s" % article_id)
+    #显示数据库中保存的文章标题和内容
+    return HttpResponse("文章标题：%s<br>文章内容：%s" % (article.title,article.intend))
+    #return HttpResponse("文章id为：%s" % article_id)
